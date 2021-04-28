15ff09b3a403ca489d5b5899426d817f506407bd
aabaroab <72941918+aabaroab@users.noreply.github.com>
2021-04-28 20:56:09 +0200

    Update test.java

--- test.java
+++ test.java
@@ -2,10 +2,6 @@ package fibonacci;
 
 class test {
 	public static void main(String args[]) {
-		int initialNumber= 0;
-		int secondNumber = 1;
-		int lastNumber;
-		int count = 10;
 		
 		System.out.println(initialNumber+" "+secondNumber);	
 		for(int i=2;i<count;i++) {
